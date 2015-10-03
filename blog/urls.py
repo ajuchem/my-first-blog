9c37f9380b192a8285d3d14f78e35a8f50e9da56
Allie Daudt <ajuchem@daudt.com>
2015-10-03 15:10:53 -0600

    Added view and template for detailed blog post as well as CSS for the site

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-	url(r'^$', views.post_list, name='post_list'),
-] #this will tell Django that views.post_list is the right place to go if someone enters http://127.0.0.1:8000/ as address
+	url(r'^$', views.post_list, name='post_list'), #this will tell Django that views.post_list is the right place to go if someone enters http://127.0.0.1:8000/ as address
+	url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
+] 
